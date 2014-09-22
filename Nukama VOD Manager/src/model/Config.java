b9bece1cb9291966013cd9be578f7a4c4acd4a30
Antoine Ceyssens <antoine.ceyssens@gmail.com>
2014-09-22 19:36:19 +0200

    Base pour le projet client applicatif
    Merci de ne jamais synchroniser les fichiers .extensions_quelconques
    afin d'éviter des problèmes de path en local. (Cela ne s'applique
    évidemment pas aux .properties)

---
+++ Nukama VOD Manager/src/model/Config.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package model;
+
+import java.util.Properties;
+
+/**
+ * Classe de gestion des configs de l'application
+ * @author Antoine
+ *
+ */
+public class Config extends Properties {
+	private static final long serialVersionUID = -7353425681698117732L;
+
+}
