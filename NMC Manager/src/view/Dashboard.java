fc116d670bc0221530827b6f7e3cdaebca8904f2
nuccah <nuccah@teamidk.eu>
2014-10-02 16:42:55 +0200

    <Manager> Continued GUI Work

--- NMC Manager/src/view/Dashboard.java
+++ NMC Manager/src/view/Dashboard.java
@@ -2,10 +2,14 @@ package view;
 
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
 import javax.swing.JTextPane;
 import javax.swing.border.EmptyBorder;
 
@@ -18,7 +22,6 @@ import javax.swing.JMenu;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.SwingConstants;
 import javax.swing.JMenuItem;
 
@@ -28,10 +31,23 @@ import controller.SessionManager;
  * @author Antoine
  *
  */
+
+class VerticalMenuBar extends JMenuBar {
+	  private static final LayoutManager grid = new GridLayout(0,1);
+	  public VerticalMenuBar() {
+	    setLayout(grid);
+	  }
+}
+
+
 public class Dashboard extends JFrame implements ActionListener{
 	private static final long serialVersionUID = -5998048938167814342L;
-	private JPanel contentPane;
-	private JMenuItem mntmQuitter;
+	private JPanel topPane;
+	private JSplitPane centerPane;
+	private JPanel leftPane;
+	private JPanel rightPane;
+	private JPanel bottomPane;
+	private BoxLayout main;
 
 	/**
 	 * Initialise la fenêtre et ses composants
@@ -39,55 +55,116 @@ public class Dashboard extends JFrame implements ActionListener{
 	public Dashboard() {
 		super(Config.getInstance().getProp("base_title")+"Nukama Media Center");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		main = new BoxLayout(getContentPane(), BoxLayout.Y_AXIS);
+		getContentPane().setLayout(main);
 		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
 		int width = gd.getDisplayMode().getWidth();
 		int height = gd.getDisplayMode().getHeight();
 		setBounds((width/2) - (width/3), (height/2) - (height/3), (int)(width/1.5), (int)(height/1.5));
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
 		
+		topPane = new JPanel();
+		topPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		topPane.setLayout(new BoxLayout(topPane, BoxLayout.X_AXIS));
+		getContentPane().add(topPane);
+		
+		leftPane = new JPanel();
+		leftPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		leftPane.setLayout(new BoxLayout(leftPane, BoxLayout.X_AXIS));
+		getContentPane().add(leftPane);
+		
+		rightPane = new JPanel();
+		rightPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		rightPane.setLayout(new BoxLayout(rightPane, BoxLayout.X_AXIS));
+		
+		getContentPane().add(rightPane);
+		
+		centerPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftPane, rightPane);
+		
+		centerPane.setDividerLocation(0.5);
+		getContentPane().add(centerPane);
+		
+		bottomPane = new JPanel();
+		bottomPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(bottomPane);
+		bottomPane.setLayout(new BoxLayout(bottomPane, BoxLayout.X_AXIS));
+		
+		titleBar();
 		menuBar();
+		scrollList();
+		bottomBar();
+	}
+
+	
+
+	/**
+	 * Barre des menus globaux
+	 */
+	public void titleBar(){
+		JMenuBar menuGlobal = new JMenuBar();
+		menuGlobal.setAlignmentY(Component.LEFT_ALIGNMENT);
+		topPane.add(menuGlobal);
 		
+		JMenuItem mnFichier = new JMenuItem("Profil");
+		mnFichier.setHorizontalAlignment(SwingConstants.LEFT);
+		menuGlobal.add(mnFichier);
+		
+		JMenuItem mnOutils = new JMenuItem("Parametres");
+		mnOutils.setHorizontalAlignment(SwingConstants.LEFT);
+		menuGlobal.add(mnOutils);
+		
+		JMenuItem mnAide = new JMenuItem("Aide");
+		mnAide.setHorizontalAlignment(SwingConstants.LEFT);
+		menuGlobal.add(mnAide);
+		
+		JMenuItem mnQuitter = new JMenuItem("Quitter");
+		mnQuitter.setHorizontalAlignment(SwingConstants.LEFT);
+		mnQuitter.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				SessionManager.getInstance().logout();
+				System.exit(0);
+			}
+		});
+		menuGlobal.add(mnQuitter);
+	}
+	
+	private void menuBar() {
+		JMenuBar menuBar = new VerticalMenuBar();
+//		menuBar.setAlignmentX(Component.LEFT_ALIGNMENT);
+//		menuBar.setAlignmentY(Component.TOP_ALIGNMENT);
+		leftPane.add(menuBar);
+		
+		JMenu mnHome = new JMenu("Home");
+		menuBar.add(mnHome);
+		
+		JMenu mnMedia = new JMenu("Media");
+		menuBar.add(mnMedia);
+		
+		JMenu mnUsers = new JMenu("User Management");
+		menuBar.add(mnUsers);
+		
+	}
+
+	private void scrollList() {
+		JScrollPane Test = new JScrollPane();
+		rightPane.add(Test);
+	}
+
+	private void bottomBar() {
 		JTextPane txtpnTest = new JTextPane();
 		txtpnTest.setEditable(false);
 		txtpnTest.setText("Salut "+Profil.getInstance().getUsername()+
 				", ton adresse mail est: "+Profil.getInstance().getMail());
-		contentPane.add(txtpnTest);
+		bottomPane.add(txtpnTest);
 	}
-	
-	/**
-	 * Barre des menus globaux
-	 */
-	public void menuBar(){
-		JMenuBar menuGlobal = new JMenuBar();
-		menuGlobal.setAlignmentY(Component.LEFT_ALIGNMENT);
-		contentPane.add(menuGlobal);
-		
-		JMenu mnFichier = new JMenu("Fichier");
-		mnFichier.setHorizontalAlignment(SwingConstants.LEFT);
-		menuGlobal.add(mnFichier);
-		
-		mntmQuitter = new JMenuItem("Quitter");
-		mntmQuitter.addActionListener(this);
-		mnFichier.add(mntmQuitter);
-		
-		JMenu mnOutils = new JMenu("Outils");
-		mnOutils.setHorizontalAlignment(SwingConstants.LEFT);
-		menuGlobal.add(mnOutils);
-		
-		JMenu mnAide = new JMenu("Aide");
-		mnAide.setHorizontalAlignment(SwingConstants.LEFT);
-		menuGlobal.add(mnAide);
-	}
+
+
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == mntmQuitter){
-			SessionManager.getInstance().logout();
-			System.exit(0);
-		}
+		// TODO Auto-generated method stub
+		
 	}
 
 }
