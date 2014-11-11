30449483fa44d355d06781173da1085da364b156
Antoine Ceyssens <antoine.ceyssens@gmail.com>
2014-11-11 14:36:20 +0100

    [Manager]
    Added frame to ask for ip when missing in the .properties
    
    GUI Design must be improved!!!!

--- NMC Manager/src/controller/Main.java
+++ NMC Manager/src/controller/Main.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 import controller.SocketManager;
 import model.Config;
 import model.Profil;
+import view.IpAsk;
 import view.Welcome;
 
 /**
@@ -17,20 +18,33 @@ public class Main {
 	public static void main(String[] args) {
 		Config.getInstance();
 		Profil.getInstance().reset();
-		//---------- Zone de config auto temporaire ---------
-		while(Config.getInstance().getProp("init").compareTo("0") == 0){
-			SocketManager.getInstance().getConfig();
-		}
-		//---------- Zone de config auto temporaire ---------
-		
-		EventQueue.invokeLater(new Runnable() {
+		//------------ IP Retrieve ----------
+		if(Config.getInstance().getProp("srv_url") == null){
+			EventQueue.invokeLater(new Runnable(){
+
+				@Override
+				public void run() {
+					new IpAsk();					
+				}				
+			});
+		} else {
+			//---------- IP Retrieve End --------
 			
-			@Override
-			public void run() {
-					Welcome wcScreen = new Welcome();
-					wcScreen.setVisible(true);								
+			//------------ Auto Config ----------
+			while(Config.getInstance().getProp("init").compareTo("0") == 0){
+				SocketManager.getInstance().getConfig();
 			}
-		});			
+			//---------- Auto Config End---------
+			
+			EventQueue.invokeLater(new Runnable() {
+				
+				@Override
+				public void run() {
+						Welcome wcScreen = new Welcome();
+						wcScreen.setVisible(true);								
+				}
+			});	
+		}
 	}
 
 }
--- NMC Manager/src/model/Config.java
+++ NMC Manager/src/model/Config.java
@@ -66,8 +66,6 @@ public class Config {
 	public void defaultConf(){
 		setProp("program_title", "Nukama MediaCenter Manager");
 		setProp("base_title", "NMC - ");
-		//TODO: Modifier gui pour demander IP et port serveur (si différent de port défaut: 50002") lors du premier lancement
-		setProp("srv_url", "localhost");
 		setProp("srv_port", "50002");
 		setProp("init", "0");
 		saveProp();
@@ -102,6 +100,30 @@ public class Config {
 			JOptionPane.showMessageDialog(null, "Error while writing configurations!");
 			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * Permet d'ajouter les propriétés passées en paramètres aux propriétés existantes
+	 * @param new_prop : Les nouvelles propriétés à ajouter
+	 */
+	public void saveProp(Properties new_prop){
+		try {
+			cfg_out = new FileOutputStream(cfg_file);
+		} catch (FileNotFoundException e1) {
+			JOptionPane.showMessageDialog(null, "The config file was not found!");
+			e1.printStackTrace();
+		}
+		try {
+			Properties merged = new Properties();
+			merged.putAll(prop);
+			merged.putAll(new_prop);
+			merged.storeToXML(cfg_out, null);
+			cfg_out.close();
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "Error while writing configurations!");
+			e.printStackTrace();
+		}
+		loadProp();
 	}
 	
 	/**
