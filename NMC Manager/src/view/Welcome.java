63764eca2edddde21c7b7c1c125265fccef50b69
Nuccah <nuccah@teamidk.eu>
2014-11-11 19:17:13 +0100

    <Manager> Purely cleanup

--- NMC Manager/src/view/Welcome.java
+++ NMC Manager/src/view/Welcome.java
@@ -43,14 +43,14 @@ public class Welcome extends JFrame implements ActionListener {
 		super(Config.getInstance().getProp("base_title")+"Bienvenue");
 		setResizable(false);
 		try {
-		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-		        if ("Nimbus".equals(info.getName())) {
-		            UIManager.setLookAndFeel(info.getClassName());
-		            break;
-		        }
-		    }
+			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
 		} catch (Exception e) {
-		    // If Nimbus is not available, you can set the GUI to another look and feel.
+			// If Nimbus is not available, you can set the GUI to another look and feel.
 		}
 		BoxLayout mainBox = new BoxLayout(getContentPane(), BoxLayout.Y_AXIS);
 		getContentPane().setLayout(mainBox);
@@ -59,69 +59,69 @@ public class Welcome extends JFrame implements ActionListener {
 		int height = gd.getDisplayMode().getHeight();
 		setBounds((width/2) - (450/2), (height/2) - (300/2), 450, 300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		URL iconURL = getClass().getResource("nmc.png");
 		ImageIcon icon = new ImageIcon(iconURL);
 		setIconImage(icon.getImage());
-		
+
 		JPanel headerPanel = new JPanel();
 		getContentPane().add(headerPanel);
 		headerPanel.setLayout(null);
-		
+
 		JLabel lblConnexion = new JLabel("Connexion");
 		lblConnexion.setBounds(166, 26, 200, 29);
 		headerPanel.add(lblConnexion);
 		lblConnexion.setFont(new Font("Tahoma", Font.PLAIN, 24));
 		lblConnexion.setAlignmentX(Component.CENTER_ALIGNMENT);
-		
+
 		JPanel centerPanel = new JPanel();
 		centerPanel.setAlignmentY(Component.BOTTOM_ALIGNMENT);
 		getContentPane().add(centerPanel);
 		centerPanel.setLayout(null);
-		
+
 		txtLogin = new JTextField();
 		txtLogin.setToolTipText("Login");
 		txtLogin.setHorizontalAlignment(SwingConstants.CENTER);
 		txtLogin.setBounds(180, 11, 140, 30);
 		centerPanel.add(txtLogin);
 		txtLogin.setColumns(10);
-		
+
 		pwdMotDePasse = new JPasswordField();
 		pwdMotDePasse.setToolTipText("Mot de Passe");
 		pwdMotDePasse.setHorizontalAlignment(SwingConstants.CENTER);
 		pwdMotDePasse.setBounds(180, 59, 140, 30);
 		centerPanel.add(pwdMotDePasse);
-		
+
 		JLabel lblLogin = new JLabel("Login :");
 		lblLogin.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblLogin.setBounds(127, 12, 50, 30);
 		centerPanel.add(lblLogin);
-		
+
 		JLabel lblMotDePasse = new JLabel("Mot de passe :");
 		lblMotDePasse.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblMotDePasse.setBounds(70, 59, 112, 30);
 		centerPanel.add(lblMotDePasse);
-		
+
 		JPanel footerPanel = new JPanel();
 		getContentPane().add(footerPanel);
 		footerPanel.setLayout(null);
-		
+
 		btnSeConnecter = new JButton("Se connecter");
 		btnSeConnecter.setBounds(25, 28, 122, 29);
 		btnSeConnecter.addActionListener(this);
 		footerPanel.add(btnSeConnecter);
-		
+
 		btnChangeIP = new JButton("Change Server IP");
 		btnChangeIP.setBounds(155, 28, 140, 29);
 		btnChangeIP.addActionListener(this);
 		footerPanel.add(btnChangeIP);
-		
+
 		btnQuitter = new JButton("Quitter");
 		btnQuitter.addActionListener(this);
 		btnQuitter.setBounds(300, 28, 122, 29);
 		footerPanel.add(btnQuitter);
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == btnQuitter){
@@ -136,20 +136,17 @@ public class Welcome extends JFrame implements ActionListener {
 						dbScreen.setVisible(true);
 					}
 				});
-				
+
 			}
-			
+
 		} else if(e.getSource() == btnChangeIP){
 			this.dispose();
 			EventQueue.invokeLater(new Runnable(){
-
-				@Override
 				public void run() {
 					IpAsk iAScreen = new IpAsk();
 					iAScreen.setVisible(true);
 				}				
 			});
 		}
-		
 	}
 }
